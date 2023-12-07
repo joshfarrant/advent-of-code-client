748acbadf00f2ff559a46b724535c4e7dccb1dfb
Josh Farrant <josh@farrant.me>
2023-12-07 14:48:24 +0000

    added redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: () => [
+    { source: '/', destination: '/2023/days', permanent: false },
+  ],
 };
 
 module.exports = nextConfig;
